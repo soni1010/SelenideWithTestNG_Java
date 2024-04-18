ba46c0dde00d02d966dc5275024fb35ccc44ec26
ayushirastogi <ayushi@svayamtech.com>
2024-04-18 19:47:01 +0530

    First Commit

---
+++ src/test/java/org/harender/AppTest.java
@@ -0,0 +1,20 @@
+package org.harender;
+
+
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
